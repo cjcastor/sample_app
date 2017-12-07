35defcfb57819b0ebfbd0634e8f0c274d8a0992a
cjcastor <cjcastor@protonmail.com>
2017-12-07 13:35:25 +0800

    finished REST actions for users

---
+++ db/migrate/20171207050234_remove_excess_passwords.rb
@@ -0,0 +1,6 @@
+class RemoveExcessPasswords < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :users, :password
+    remove_column :users, :password_confirmation
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205120932) do
+ActiveRecord::Schema.define(version: 20171207050234) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password"
-    t.string "password_confirmation"
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
